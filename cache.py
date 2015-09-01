3d1625526f142235d3d9c8d5817c07b0cdf92826
Gennadii <g.mishchevskii@gmail.com>
2015-09-01 18:09:03 +0300

    Create cache.py

---
+++ cache.py
@@ -0,0 +1,17 @@
+import shutil
+import os
+
+count = 0
+exceptions = ['data_0','data_1','data_2','data_3','index']
+os.chdir(os.path.expanduser(r'~\AppData\Local\Google\Chrome\User Data\Default\Cache'))
+files = os.listdir()
+
+for filee in files:
+	if filee not in exceptions:
+		os.remove(os.path.expanduser(r'~\AppData\Local\Google\Chrome\User Data\Default\Cache\\' + filee))
+		count += 1
+
+if len(os.listdir()) == 5:
+	print('\n' + str(count) + ' files have been deleted')
+
+exit()
